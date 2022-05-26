9209f129a638b6343afddd2fa9cf302eedc6fe24
joan kenedi mayo <talentojkm@gmail.com>
2022-05-25 22:17:41 -0500

    heroku

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 // Routers
 const { usersRouter } = require('./routes/users.routes');
 const { repairsRouter } = require('./routes/repairs.routes');
 
 const { globalErrorHandler } = require('./controllers/error.controller');
-const compression = require('compression');
-const { default: helmet } = require('helmet');
 
 const app = express();
 
 // Enable incoming JSON data
 app.use(express.json());
 
-// Endpoints
-app.use('/api/v1/users', usersRouter);
-app.use('/api/v1/repairs', repairsRouter);
-
 app.use(helmet());
 app.use(compression());
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 else app.use(morgan('combined'));
+
+// Endpoints
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/repairs', repairsRouter);
+
 // Global error handler
 app.use('*', globalErrorHandler);
 
